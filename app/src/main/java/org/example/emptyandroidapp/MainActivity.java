d2aa3bb0bc10ebeb49ec52159a88b0ddf5e86c4a
jakub-g <jakub.g.opensource@gmail.com>
2017-06-21 14:50:08 +0200

    Bug repro

--- app/src/main/java/org/example/emptyandroidapp/MainActivity.java
+++ app/src/main/java/org/example/emptyandroidapp/MainActivity.java
@@ -1,7 +1,8 @@
 package org.example.emptyandroidapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myTestMethod();
+    }
+
+    private boolean myTestMethod() {
+        int myTest = 42;
+        if ("ABCD".equals("AB" + "CD")) {
+            myTest = 666;
+            Log.v("TEST", "true");
+            Log.v("TEST", String.valueOf(myTest));
+            return true;
+        } else {
+            Log.v("TEST", "false");
+            Log.v("TEST", String.valueOf(myTest));
+            return false;
+        }
     }
 }
